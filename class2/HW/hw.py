c7596db75a3f57c6f5160991c2edb7f10c932a43
mark-chen981023 <markchentw98@gmail.com>
2022-09-14 19:25:02 +0800

    完成作業

---
+++ class2/HW/hw.py
@@ -0,0 +1,20 @@
+'''
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+
+int  :apple、age
+bool :minor
+str  :name zip
+float:weight
+
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+'''
