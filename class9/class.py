a70a83dca02fec9ec72c74ad4dbe36829c2a6500
mark-chen981023 <markchentw98@gmail.com>
2022-11-09 19:33:27 +0800

    finished

---
+++ class9/class.py
@@ -0,0 +1,36 @@
+# i = 1
+# while i < 6:
+#     if i == 3:
+#         break
+#     print(i)
+# else:
+#     ('迴圈正常')
+# print(456)
+# i = 0
+# while i != '4':
+#     print('1. 蘋果汁\n2. 柳橙汁\n3. 葡萄汁\n4. 系統關閉')
+#     i = input('請輸入編號:')
+#     if i == '1':
+#         print('您點的商品是蘋果汁')
+#     elif i == '2':
+#         print('您點的商品是柳橙汁')
+#     elif i == '3':
+#         print('您點的商品是葡萄汁')
+#     elif i == '4':
+#         break
+#     else:
+#         print('輸入錯誤查無此果汁，請重新輸入')
+# print('~~系統關閉~~')
+# import random as r
+
+# i = r.randrange(1, 101)
+# x = 0
+# while x != i:
+#     x = int(input('猜一個數字'))
+#     if x > i:
+#         print('小一點')
+#     elif x < i:
+#         print('大一點')
+#     else:
+#         print('猜中了')
+#         break
