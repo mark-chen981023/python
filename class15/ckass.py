4ee2d480f58e0acfbce4b784257a444b4f8f14bf
mark-chen981023 <markchentw98@gmail.com>
2022-12-28 19:01:00 +0800

    finished

---
+++ class15/ckass.py
@@ -0,0 +1,18 @@
+# def cry():
+#     print('cry')
+
+# cry()
+# def hello(name):
+#     print(f"hello{name}")
+
+
+# hello('b')
+def hello(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+
+x = hello(3, 2)
+print('bye', x)
